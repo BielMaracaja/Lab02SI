dc15eda49a6b6d0ee39b03d5343d1f8905647cae
Gabriel <gabriel.maracaja@ccc.ufcg.edu.br>
2016-12-09 19:16:23 -0300

    Primeiro commit

---
+++ js/controller.js
@@ -0,0 +1,58 @@
+angular.module("ListaDeTarefas").controller("listaDeTarefasCtrl", function ($scope){
+
+    $scope.app = "Lista de Tarefas";
+
+    $scope.tarefas = [
+        {tarefa: "Estudar SI", numero: "1"},
+        {tarefa: "Arrumar a Casa", numero: "2"},
+        {tarefa: "Comprar pão", numero: "3"}
+    ];
+
+    $scope.adicionarTarefa = function (tarefa){
+        $scope.tarefas.push(angular.copy(tarefa));
+        delete $scope.tarefa;
+    };
+
+    $scope.classe = "selecionado";
+
+    $scope.apagarTarefa = function (tarefas) {
+        $scope.tarefas = tarefas.filter(function (tarefa){
+            if(!tarefa.selecionado) return tarefa;
+        });
+    };
+
+    $scope.isTarefaSelecionado = function (tarefas) {
+        return tarefas.some(function (tarefa) {
+            return tarefa.selecionado;
+        });
+    };
+
+    $scope.quantidadeTarefas = function (tarefas) {
+        return $scope.tarefas.length;
+    };
+
+    $scope.tarefasRealizadas = function () {
+        var tarefasRealizadas = 0;
+        for(var i = 0; i < $scope.tarefas.length; i++){
+            if($scope.tarefas[i].selecionado) tarefasRealizadas++;
+        }
+        return tarefasRealizadas;
+    };
+
+    $scope.barraDePorcentagem = function (){
+        if(!$scope.listaNaoVazia()){
+            return 0;
+        }
+        var percentual = ($scope.tarefasRealizadas() * 100) / ($scope.tarefas.length);
+        return parseInt(percentual);
+    };
+
+    $scope.listaNaoVazia = function (){
+        return $scope.tarefas.length > 0;
+    };
+
+    $scope.apagarTudo = function (){
+        $scope.tarefas = [];
+    }
+
+});
